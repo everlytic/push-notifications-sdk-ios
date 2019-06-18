ad40ca1fd86c4567e5dac1f9ac1345758e6dcd56
Jason Dantuma <jason@dantuma.co.za>
2019-06-18 14:05:46 +0200

    Initial Commit

---
+++ EverlyticPush/EverlyticPush.h
@@ -0,0 +1,19 @@
+//
+//  EverlyticPush.h
+//  EverlyticPush
+//
+//  Created by Jason Dantuma on 2019/06/18.
+//  Copyright © 2019 Everlytic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EverlyticPush.
+FOUNDATION_EXPORT double EverlyticPushVersionNumber;
+
+//! Project version string for EverlyticPush.
+FOUNDATION_EXPORT const unsigned char EverlyticPushVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EverlyticPush/PublicHeader.h>
+
+
